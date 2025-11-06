5283a60b6739f0ae6fd0fa8f3866e3879ab6fa15
Chindanai 66102122108 <chindanai.su66@snru.ac.th>
2025-11-06 09:11:33 +0700

    นายคำสะหวัน หลวงลาด 66102122115

---
+++ app/src/main/java/com/example/androidfragmenthw/A66102122115Activity.kt
@@ -0,0 +1,13 @@
+package com.example.androidfragmenthw
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class A66102122115Activity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_66102122115)
+
+        title = "66102122115Activity"
+    }
+}
